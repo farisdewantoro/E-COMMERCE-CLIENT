8ec86c42a736baff22f6c11300143f374cbdb82e
Faris Dewantoro <farisd117@gmail.com>
2019-09-17 18:55:17 +0700

    2

--- server/config/keys-production.js
+++ server/config/keys-production.js
@@ -1,62 +1,71 @@
 // add this file to .gitignore
 import instagramKey from './instagram.json';
-export default {
-    google: {
-        clientID: "1066262252537-9k2ju0c1iuifkjlmtf33mi7b53jl5hbp.apps.googleusercontent.com",
-        clientSecret: '-7WBqU0ePK8dHK00hAPg1zw-'
+export default  {
+    google:{
+        clientID: "888048154617-0hauahb913ftvmocigbbgdg5t104rut3.apps.googleusercontent.com",
+        clientSecret: 'Jy8fOcnNSu9OdA5FqWfTVFKc'
     },
-    facebook: {
-        clientID: "364630457710524",
-        clientSecret: "f7401af34857feb4ccb15c6e69aad42f"
+    facebook:{
+        clientID: "2353590754664028",
+        clientSecret:"fc2d9afea09585c7cd6ea75db8d8a378"
     },
-    session: {
-        cookieKey: "iCtkGVe0-15asd12315sax-xJkLm2K2220XlkD",
-        name: "hammerstout_s",
-        secret: "iCtkGVe0-4FKIGgBopL2QUM9K-jIK9miZhQExxxx2xuxxxx",
+    session:{
+        cookieKey:"iCtkGVe0-15asd12315sax-xJkLm2K2220XlkD",
+        name:"hammerstout_s",
+        secret:"iCtkGVe0-4FKIGgBopL2QUM9K-jIK9miZhQExxxx2xuxxxx",
         maxAge: 24 * 60 * 60 * 1000
     },
-    jwt: {
-        secretOrPrivateKey: "AAAABB3L-X59kbcuqwzxc--23kv,df594.41239zsc92231",
+    jwt:{
+        secretOrPrivateKey:"AAAABB3L-X59kbcuqwzxc--23kv,df594.41239zsc92231",
         secretOrPrivateKey2: "2DCtkGVe-jifmqs53v6sbgg05u3fkbcDuDxDDqwzD2xc--23kv,df594.0ut79s41qi0lhg",
         secretOrPrivateKey3: "X59kbcuqwzxc-VcMFhXkPjVaIjz--23kv,df594.df594xxx",
-        expiresIn: '2d'
+        expiresIn:'2d' 
     },
-    origin: {
-        url:"https://hammerstoutdenim.com",
-        redirectProvider:"https://hammerstoutdenim.com",
-        redirectLogin: "https://hammerstoutdenim.com/sign-in",
-        redirect: "https://hammerstoutdenim.com/carts",
-        redictProfile: "https://hammerstoutdenim.com/my-account"
+    origin:{
+        url:"https://hammerstout-client.herokuapp.com",
+        redirectProvider: "https://hammerstout-admin.herokuapp.com/", //DEV
+        // redirectProvider: "http://localhost:3000", //PROD LOCAL
+        redirectLogin:"https://hammerstout-client.herokuapp.com/sign-in",
+        redirect:"https://hammerstout-client.herokuapp.com/carts",
+        redictProfile:"https://hammerstout-client.herokuapp.com/my-account",
+        confirmPay:"https://hammerstout-client.herokuapp.com/confirm-payment"
     },
     rajaongkir: {
-        key: "850129b7327c8206f3875333eb281f0e",
+        key: process.env.RAJA_ONGKIR_KEY,
         originId: 23,
-        name: "bandung"
+        name: "bandung",
     },
     rajasms: {
-        key: "457f38ca6a3265ff7ee62df327d70854",
-        username: "hammerstoutdenim"
+        key: process.env.RAJA_SMS,
+        username: process.env.RAJA_SMS_USERNAME
     },
     midtrans: {
-        url: "https://api.midtrans.com",
-        id: "G592798515",
-        clientKey: "Mid-client-Wn88s0ecuXz5VQoE",
-        serverKey: "Mid-server-JKiiGZJ6mH39aIrEZGCLjWWA",
-        isProduction: true
+        url: "https://api.sandbox.midtrans.com",
+        id: process.env.MIDTRANS_ID,
+        clientKey: process.env.MIDTRANS_CLIENT_KEY,
+        serverKey: process.env.MIDTRANS_SERVER_KEY,
+        isProduction: false
+    },
+    mode:{
+        active:true,
+        key:"2DCtkGVe-x6789jifmqs53v%^&@$6sbgg05u3fkbcDuDxDDqwzD2xc--23kv,df594.0ut79s41qi0lhg"
     },
     instagram: {
         access_token: instagramKey.access_token,
-        clientID: '02fb6221724249ef9df4af191679b6f9',
-        clientSecret: 'ef08d2d4b38a47939d6ebd7167ba49e6'
+        clientID: process.env.INSTAGRAM_ID,
+        clientSecret: process.env.INSTAGRAM_SECRET,
+        redirect: 'https://hammerstout-admin.herokuapp.com/api/instagram/refresh/token'
     },
     database: {
-        host:'localhost',
-        user:'hammerst_denim',
-        password:'6!5jat!e8iLQ',
-        database:'hammerst_hammer',
-    },
-    mode: {
-        active: true,
-        key: "2DCtkGVe-x6789jifmqs53v%^&@$6sbgg05u3fkbcDuDxDDqwzD2xc--23kv,df594.0ut79s41qi0lhg"
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE
     }
 };
+
+
+
+
+
+
